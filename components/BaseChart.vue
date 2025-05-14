2bbb2b5629ba3f2d8f122e5963ac9e7a2239f7a0
Abdulhamid Alabbad <alabbad.2021@gmail.com>
2025-05-14 11:13:54 +0300

    add baseChart component

---
+++ components/BaseChart.vue
@@ -0,0 +1,47 @@
+<template>
+    <component :is="type" :data="props.data" :options="props.options" />
+</template>
+
+<script setup lang="ts">
+import { Bar, Line, Scatter } from 'vue-chartjs'
+import {
+    Chart as ChartJS,
+    Title,
+    Tooltip,
+    Legend,
+    BarElement,
+    LineElement,
+    PointElement,
+    CategoryScale,
+    LinearScale
+} from 'chart.js'
+
+// Import the datalabels plugin
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+
+// Register the necessary Chart.js components for a Line Chart
+ChartJS.register(CategoryScale, LinearScale, BarElement, LineElement, PointElement, Title, Tooltip, Legend, ChartDataLabels);
+
+const type = computed(() => {
+    switch (props.type.toLowerCase()) {
+        case 'bar':
+            return Bar;
+        case 'line':
+            return Line;
+        case 'scatter':
+            return Scatter;
+        default:
+            throw new Error('Invalid chart type');
+    }
+});
+
+// Define props for chart data and options
+// Using `any` for simplicity in this example, but you might want more specific types
+// based on Chart.js typings or your own data structure.
+const props = defineProps<{
+  data: any;
+  options?: any; // Optional prop for chart options
+  type: 'bar' | 'line' | 'scatter'; // Specify the type of chart
+}>();
+
+</script>
